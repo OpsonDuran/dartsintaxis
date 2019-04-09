610bbd56b763008a79117ab7a7084b5a22f16683
armando.duran2 <armando.duran2@bbva.com>
2019-04-08 22:51:52 -0500

    add dart basics var types

---
+++ basics.dart
@@ -0,0 +1,55 @@
+
+//En dart todo es un Objeto 
+
+//Var es no fuertemente tipada 
+
+var variablecomun = 'dash';
+
+//Dynamic acepta cualquier tipo de dato y cambiarán según la necesidad en su uso futuro
+
+dynamic variabledinamica = 'dash';
+
+// const debe tener asignado el valor en tiempo de compilación y final en tiempo de ejecución, se usa para valores fijos que no cambiaran
+
+final contantefinal = 'soy una constante typo final';
+
+const constante = 'soy una constante typo const';
+
+//Variables deacuerdo a su tipo de dato:
+
+// Strings: '' y "" son equivalentes
+
+String palabra = 'palabra';
+
+String palabracomillas = "palabra";
+
+String textoMultilinea = '''un texto
+multilinea''';
+
+String textoMultilineacomillas = """otro texto
+multilinea""";
+
+// Numeros Enteros
+
+int x = 10;
+
+// Numeros Decimales
+
+double y = 20.67;
+
+// Booleano true o false
+
+bool flag = true;
+
+// Lista
+
+var lista = [0,1,2,3,4,5,6];
+
+// Mapa 
+
+var dias = {
+// Llave: Valor
+  'Lu': 'Lunes',
+  'Ma': 'Martes',
+  'Mi': 'Miercoles'
+};
